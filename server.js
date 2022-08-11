3bd51f48158b970e73640c5cf6b268825e893fa1
pb-wd027 <pbauer@protonmail.com>
2022-08-11 04:17:50 +0200

    DB connection fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const path = require("path");
+
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
@@ -14,13 +14,6 @@ app.use("/api/doctor", doctorRoute);
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-
-    app.use("/", express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "appointment-service-frontend/build/index.html"));
-    });
-  }
   
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
   
