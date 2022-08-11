7e37cd7379feefcc06eeabecfd34acc981a3626e
pb-wd027 <pbauer@protonmail.com>
2022-08-11 15:39:06 +0200

    server network config

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
   
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
